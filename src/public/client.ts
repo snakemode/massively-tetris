6c15222210d6a4c1cb9257127b97a6d98f0f7e67
Glitch (massively-tetris) <none>
2020-04-20 12:34:28 +0000

    🏝🐙 Checkpoint ./src/public/render.ts:7701387/2287 ./src/public/client.ts:7701387/802

--- src/public/client.ts
+++ src/public/client.ts
@@ -3,33 +3,7 @@ import { World } from "../tetris/World";
 import { uuidv4 } from "../util";
 import { Move } from "../tetris/Types";
 import { Controls } from "./Controls";
-
-const canvas: any = document.getElementById("game");
-var ctx = canvas.getContext("2d");
-
-const minoSize = 30;
-
-function render(game: Tetris) {
-  let index = -1;
-  
-  for (const row of game.world.rows()) {        
-    index++;
-    const offsetY = index * minoSize;
-
-    let cellIndex = -1;
-    
-    for (const cell of row) {
-      cellIndex++;
-      const offsetX = cellIndex * minoSize;
-           
-      ctx.fillStyle = "black";
-      ctx.fillRect(offsetX, offsetY, minoSize, minoSize);            
-
-      ctx.fillStyle = cell.occupied ? "red" : "white";
-      ctx.fillRect(offsetX + 1, offsetY + 1, minoSize - 2, minoSize - 2);    
-    }        
-  }
-}
+import { render } from "./render";
 
 let game: Tetris;
 let controls: Controls;
---
+++ src/public/render.ts
@@ -0,0 +1,46 @@
+import { Tetris } from "../tetris/Tetris";
+import { World } from "../tetris/World";
+import { Move, Mino, ValidTetronimo } from "../tetris/Types";
+
+const canvas: any = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+const minoSize = 30;
+
+export function render(game: Tetris) {
+  let index = -1;
+  
+  for (const row of game.world.rows()) {        
+    index++;
+    const offsetY = index * minoSize;
+
+    let cellIndex = -1;
+    
+    for (const cell of row) {
+      cellIndex++;
+      const offsetX = cellIndex * minoSize;
+           
+      ctx.fillStyle = "black";
+      ctx.fillRect(offsetX, offsetY, minoSize, minoSize);            
+
+      ctx.fillStyle = selectColour(cell);
+      ctx.fillRect(offsetX + 1, offsetY + 1, minoSize - 2, minoSize - 2);    
+    }        
+  }
+}
+
+function selectColour(mino: Mino): string {
+  if (!mino.occupied) {
+    return "white";
+  }  
+  switch(mino.shape) {
+    case "I": return "powderBlue";
+    case "J": return "powderBlue";
+    case "L": return "powderBlue";
+    case "O": return "powderBlue";      
+    case "S": return "powderBlue";      
+    case "T": return "powderBlue";            
+    case "Z": return "powderBlue";
+    case default: return "black";
+  }
+}
