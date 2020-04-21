a7610282b73d589fcedb87509b04eef0ec4671cc
Glitch (massively-tetris) <none>
2020-04-21 15:27:04 +0000

    🐧🌋 Checkpoint ./src/tetris/World.test.ts:7701387/8117

--- src/tetris/World.test.ts
+++ src/tetris/World.test.ts
@@ -1,6 +1,5 @@
-import { World } from "./World";
+import { Location, RotationOperation } from './Types';
 import { WorldFactory } from './WorldFactory';
-
 
 describe("World", () => {
 
@@ -90,6 +89,102 @@ describe("World", () => {
                     "S   ",
                 ]);
             });
+        });
+
+    });
+
+    describe("Move", () => {
+
+        it("Can move horizontally", () => {
+            const sut = WorldFactory.fromState([
+                "   ss     ",
+                "IIss      ",
+            ], { x: 2, y: 1}, "S");
+
+            sut.move({ deltaX: 1, deltaY: 0, rotation: RotationOperation.None });
+
+            const snapshot = sut.toStringArray(true);
+            expect(snapshot).toStrictEqual([
+                "    SS    ",
+                "II SS     ",
+            ]);
+        });        
+
+        it("Won't let a tetromino move through a neighbour horizontally", () => {
+            const sut = WorldFactory.fromState([
+                "   ss     ",
+                "IIss      ",
+            ], { x: 2, y: 1}, "S");
+
+            sut.move({ deltaX: -1, deltaY: 0, rotation: RotationOperation.None });
+
+            const snapshot = sut.toStringArray(true);
+            expect(snapshot).toStrictEqual([
+                "   SS     ",
+                "IISS      ",
+            ]);
+        });
+
+        it("Won't let a tetromino move through the left wall", () => {
+            const sut = WorldFactory.fromState([
+                " ss     ",
+                "ss      ",
+            ], { x: 0, y: 1}, "S");
+
+            sut.move({ deltaX: -1, deltaY: 0, rotation: RotationOperation.None });
+
+            const snapshot = sut.toStringArray(true);
+            expect(snapshot).toStrictEqual([
+                " SS     ",
+                "SS      ",
+            ]);
+        });
+
+        it("Won't let a tetromino move through the right wall", () => {
+            const sut = WorldFactory.fromState([
+                "  ss",
+                " ss ",
+            ], { x: 1, y: 1}, "S");
+
+            sut.move({ deltaX: 1, deltaY: 0, rotation: RotationOperation.None });
+
+            const snapshot = sut.toStringArray(true);
+            expect(snapshot).toStrictEqual([
+                "  SS",
+                " SS ",
+            ]);
+        });
+
+        it("Won't let a tetromino move through the floor", () => {
+            const sut = WorldFactory.fromState([
+                " ss",
+                "ss ",
+            ], { x: 0, y: 1}, "S");
+
+            sut.move({ deltaX: 0, deltaY: -1, rotation: RotationOperation.None });
+
+            const snapshot = sut.toStringArray(true);
+            expect(snapshot).toEqual([
+                " SS",
+                "SS ",
+            ]);
+        });
+
+        it("Won't let a tetromino move through a piece underneath it", () => {
+            const sut = WorldFactory.fromState([
+                " ss",
+                "ss ",
+                "III",
+            ], { x: 0, y: 2}, "S");
+
+            sut.move({ deltaX: 0, deltaY: -1, rotation: RotationOperation.None });
+
+            const snapshot = sut.toStringArray(true);
+            expect(snapshot).toEqual([
+                " SS",
+                "SS ",
+                "III",
+            ]);
         });
 
     });
