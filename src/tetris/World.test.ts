ce6fe959f2e0a2d1f06a94bcb1374c6180c1afe8
Glitch (massively-tetris) <none>
2020-04-21 15:39:05 +0000

    🎲🗃 Checkpoint ./src/tetris/World.test.ts:7701387/33403 ./src/tetris/WorldFactory.ts:7701387/1976

--- src/tetris/World.test.ts
+++ src/tetris/World.test.ts
@@ -3,55 +3,40 @@ import { WorldFactory } from './WorldFactory';
 
 describe("World", () => {
 
-    it("Can make a test world from state", () => {
-        const sut = WorldFactory.fromState([
-            "S   ",
-            "IIII",
-            "IIII"
-        ]);
-        const snapshot = sut.toStringArray();
-        expect(snapshot).toStrictEqual([
-            "S   ",
-            "IIII",
-            "IIII"
-        ]);
-    });
-
     describe("Tick", () => {
 
         describe("clears completed rows", () => {
 
             it("when there is a single complete row", () => {
                 const sut = WorldFactory.fromState([
-                    "          ",
-                    "IIIIIIIIII"
-                ]);
-
-                sut.tick();
-
-                const snapshot = sut.toStringArray();
-                expect(snapshot[1]).toBe("          ");
-
-            });
-
-            it("when there are multiple complete rows", () => {
-                const sut = WorldFactory.fromState([
-                    "          ",
-                    "          ",
-                    "          ",
-                    "IIIIIIIIII",
-                    "IIIIIIIIII"
+                    "     ",
+                    "IIIII"
                 ]);
 
                 sut.tick();
 
                 const snapshot = sut.toStringArray();
                 expect(snapshot).toStrictEqual([
-                    "          ",
-                    "          ",
-                    "          ",
-                    "          ",
-                    "          ",
+                    "     ",
+                    "     "
+                ]);
+
+            });
+
+            it("when there are multiple complete rows", () => {
+                const sut = WorldFactory.fromState([
+                    "     ",
+                    "IIIII",
+                    "IIIII"
+                ]);
+
+                sut.tick();
+
+                const snapshot = sut.toStringArray();
+                expect(snapshot).toStrictEqual([
+                    "     ",
+                    "     ",
+                    "     "
                 ]);
             });
         });
@@ -60,16 +45,16 @@ describe("World", () => {
 
             it("contents fall downwards when clearing single row", () => {
                 const sut = WorldFactory.fromState([
-                    "S         ",
-                    "IIIIIIIIII"
+                    "S   ",
+                    "IIII"
                 ]);
 
                 sut.tick();
 
                 const snapshot = sut.toStringArray();
                 expect(snapshot).toStrictEqual([
-                    "          ",
-                    "S         ",
+                    "    ",
+                    "S   "
                 ]);
             });
 
@@ -86,6 +71,25 @@ describe("World", () => {
                 expect(snapshot).toEqual([
                     "    ",
                     "    ",
+                    "S   ",
+                ]);
+            });
+
+            it("multiple rows of contents fall downwards when clearing multiple rows", () => {
+                const sut = WorldFactory.fromState([
+                    "S   ",
+                    "S   ",
+                    "IIII",
+                    "IIII"
+                ]);
+        
+                sut.tick();
+        
+                const snapshot = sut.toStringArray();
+                expect(snapshot).toEqual([
+                    "    ",
+                    "    ",
+                    "S   ",
                     "S   ",
                 ]);
             });
@@ -186,8 +190,22 @@ describe("World", () => {
                 "III",
             ]);
         });
+        
+        it("Supports rotation", () => {
+            const sut = WorldFactory.fromState([
+                " ss",
+                "ss ",
+                "   ",
+            ], { x: 0, y: 1}, "S");
 
+            sut.move({ deltaX: 0, deltaY: 0, rotation: RotationOperation.Right });
+
+            const snapshot = sut.toStringArray(true);
+            expect(snapshot).toEqual([ 
+                'S  ', 
+                'SS ', 
+                ' S ' 
+            ]);
+        });
     });
-
-
 });
--- src/tetris/WorldFactory.ts
+++ src/tetris/WorldFactory.ts
@@ -1,23 +1,38 @@
 
-import { ValidTetronimo } from './Types';
+import { ValidTetronimo, ValidTetronimos, Location, Mino } from './Types';
 import { World } from "./World";
+import { Tetromino } from './Tetromino';
 
 export class WorldFactory {  
-    public static fromState(worldLayout: string[]): World {
+
+    public static fromState(worldLayout: string[], activeTetronimoTopLeft?: Location, shape?: ValidTetronimo): World {
       worldLayout = worldLayout.reverse();
       var width = worldLayout[0].length;
       var height = worldLayout.length;
   
       var world = new World("player1", width, height);
   
-      for (const [y, line] of worldLayout.entries()) {
+      for (const _y in worldLayout) {
+        const y = parseInt(_y);  
+        const line = worldLayout[y]
         const cells = line.split('');
         
-        for (const [x, cell] of cells.entries()) {
-          if (cell != ' ') {
-            world.occupiedLocations.push({x, y, shape: cell as any as ValidTetronimo });
+        for (const _x in cells) {  
+          const x = parseInt(_x);          
+          const cell: string = cells[x];
+
+          if (cell != ' ' && ValidTetronimos.indexOf(cell as any) != -1) {
+            const mino: Mino = {x, y, shape: cell as any as ValidTetronimo };
+            world.occupiedLocations.push(mino);
           }
         }
+      }
+
+      if (activeTetronimoTopLeft) {       
+        const type = shape || "_";
+        world.tetromino = Tetromino.create(type);
+        world.tetromino.location.x = activeTetronimoTopLeft.x;
+        world.tetromino.location.y = activeTetronimoTopLeft.y;
       }
   
       return world;
